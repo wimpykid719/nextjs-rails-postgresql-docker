5e60ac6d84ddf862109eedfdc33a73fcf2fac3b3
wataru <ltomu.web@gmail.com>
2024-01-28 20:28:40 +0900

    fix: フロントエンドの環境アップデート
    imageのnodeバージョン上げ
    COPYコマンドの仕様が変更されワイルドカードが仕様不可になったため
    create-next-appで作成されるフォルダをあらかじめ作成することにした
    空のフォルダはgit add 出来ないため .gitkeep を使用
    
    環境構築後開発が進んでも環境構築出来るようにnpm installの部分を修正
    create-next-appのオプションを最新のバージョン14に対応できるように修正

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -5,16 +5,17 @@ if [ ! -e "/frontend/package.json" ]; then
   echo 'nextjsを新規インストール'
   npm init -y
   npm install create-next-app
-  npx create-next-app@latest neumann-client --use-npm --typescript
+  npx create-next-app@latest neumann-client --ts --tailwind \
+    --no-eslint --app --src-dir --import-alias '@/*'
   rm -rf neumann-client/.gitignore
+  cd neumann-client
 fi
 
 if [ ! -d "/frontend/neumann-client/node_modules" ]; then
   echo 'neumann-clientの環境構築'
+  cd neumann-client
   npm install
 fi
-
-cd neumann-client
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
